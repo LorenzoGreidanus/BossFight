f9caf9df22898b396a45dd71a69e0f02f6f52f56
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2019-07-02 10:15:17 +0200

    Player movement/stats

---
+++ BossFight/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float dash;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float maxDashCooldown;
+    [SerializeField] private float dashStopMultiplier;
+    [SerializeField] private float movementSpeed;
+
+    public float dashCooldown;
+    public float dashStamina;
+
+    private CharacterController charController;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+        Dash();
+    }
+
+    private void PlayerMovement()
+    {
+        float verInput = Input.GetAxis(verticalInputName) * movementSpeed * dash;
+        float horInput = Input.GetAxis(horizontalInputName) * movementSpeed * dash;
+
+        Vector3 forwardMovement = transform.forward * verInput;
+        Vector3 rightMovement = transform.right * horInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+    }
+
+    private void Dash()
+    {
+        if (Input.GetButton("Dash") && dashCooldown <= 0 && gameObject.GetComponent<PlayerStats>().stamina != 0 && gameObject.GetComponent<PlayerStats>().staminaPenalty == false)
+        {
+            dash *= dashSpeed;
+            dashCooldown = maxDashCooldown;
+            gameObject.GetComponent<PlayerStats>().stamina -= dashStamina;
+        }
+
+        dashCooldown -= Time.deltaTime;
+        if (dash > 1)
+        {
+            dash -= Time.deltaTime * dashStopMultiplier;
+        }
+        else
+        {
+            dash = 1;
+        }
+    }
+}
---
+++ BossFight/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float health;
+    public float stamina;
+    public bool staminaPenalty;
+    public float staminaDebuff;
+
+    public Image healthSlider;
+    public Image staminaSlider;
+
+    private void Awake()
+    {
+        staminaPenalty = false;
+    }
+
+    public void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        if (stamina != 100 && gameObject.GetComponent<PlayerMove>().dashCooldown <= 0)
+        {
+            StaminaRegen();
+        }
+        if (stamina < 0)
+        {
+            staminaPenalty = true;
+        }
+        else if (stamina > staminaDebuff)
+        {
+            staminaPenalty = false;
+        }
+
+        healthSlider.fillAmount = health / 100f;
+        staminaSlider.fillAmount = stamina / 100f;
+    }
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+    }
+
+    public void StaminaRegen()
+    {
+        stamina += 10 * Time.deltaTime;
+    }
+}
